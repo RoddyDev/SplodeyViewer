b8c6bbc3657755b2b13fdda4320710adb525d819
roddy <45442056+RoddyDev@users.noreply.github.com>
2025-05-13 00:05:19 -0300

    v1.1.4 support
    This update adds support for the latest v1.1.4 patch.

--- SplodeyViewerTiny.cs
+++ SplodeyViewerTiny.cs
@@ -35,7 +35,7 @@ namespace SplodeyViewerTiny
                 while (startReading)
                 {
                     baseAddr = processHandler.GetModuleBase("Splodey.exe");
-                    room_id = processHandler.ReadInt(baseAddr, 0xC2C498); // For version 1.1.2
+                    room_id = processHandler.ReadInt(baseAddr, 0xBB1880); // For version 1.1.4
                     if (room_id != old_room_id)
                     {
                         try
